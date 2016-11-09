6070eface724a16a4ba32e9a9ddb7eba3703cb14
SNelzz <niels.daalhuisen@student.hu.nl>
2016-11-09 15:39:26 +0100

    Threading toegevoegd

--- nsAPI/nsAPI.py
+++ nsAPI/nsAPI.py
@@ -1,5 +1,6 @@
 import requests
 import xmltodict
+import threading
 
 auth_details = ('tjibbe.vanderende@student.hu.nl', 'yp9PbnRHnhWx-gBERapPKMy1o792T6U20D9Xw2W47xr8fvek-TvS9g')
 api_vertrek = 'http://webservices.ns.nl/ns-api-avt?station='
@@ -17,8 +18,9 @@ def vertrektijden(station):
     with open('vertrektijden.xml', 'w') as myXMLFile:
         myXMLFile.write(vertrektijd.text)
 
-    return True
+station = str(input('Welk station wilt u de informatie van hebben? '))
+vertrekThread = threading.Thread(name=vertrektijden, target=vertrektijden(station))
+stationThread = threading.Thread(name=stationlijst, target=stationlijst)
 
-station = input('Welk station wilt u de informatie van hebben? ')
-vertrektijden(station)
-stationlijst()
+vertrekThread.start()
+stationThread.start()
